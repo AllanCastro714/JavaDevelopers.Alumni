a0cf04ad69218f81fab94ad65adb8e3688e4cb49
Allan L. Castro <107632079+AllanCastro714@users.noreply.github.com>
2022-07-02 16:41:14 -0300

    Add files via upload

---
+++ desafio001.java
@@ -0,0 +1,44 @@
+public class desafio001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated methodSystem.out.println
+
+		
+			/* Variables Primitivas en Java
+			   Tipo numerico entero
+			   byte: 8 bits de almacenamiento | valores en el rango de [-128, 127]
+			*/
+			byte unByte = 8;
+			System.out.println(unByte);
+
+			//short: 2 bytes de almacenamiento | valores en el rango de [-32.768, 32.767]
+			short unShort = 11589;
+			System.out.println(unShort);
+
+			//int
+			int unInt = 12;
+			System.out.println(unInt);
+
+			//long
+			long unLong = 1000000000;
+			System.out.println(unLong);
+
+			//Tipo numerico decimal
+			//Float
+			float unFloat = 10;
+			System.out.println(unFloat);
+			
+			//double
+			double unDouble = 1020.3265;
+			System.out.println(unDouble);
+
+			//Booleanos y caracteres
+			//boolean
+			boolean unBoolean = true;
+			System.out.println(unBoolean);
+
+			//string
+			String unString = "Hola Mundo";
+			System.out.println(unString);
+	}
+}
---
+++ desafio002.java
@@ -0,0 +1,73 @@
+
+public class desafio002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	System.out.println("A");
+
+		int x = 10;
+		int y = 20;
+
+	System.out.println(x);
+	System.out.println(y);
+
+	/*Resultado en consola A:
+		x = 10
+		y = 20
+	*/
+
+	System.out.println("B");
+
+		x = x + 5;
+		y = y + 10;
+
+	System.out.println(x);
+	System.out.println(y);
+
+	/*Resultado en consola B:
+		x = 15
+		y = 30
+	*/
+
+	System.out.println("C");
+
+		x = x - 5;
+		y = y - 10;
+
+	System.out.println(x);
+	System.out.println(y);
+
+	/*Resultado en consola C:
+		x = 5
+		y = 10
+	*/
+
+	System.out.println("D");
+
+		x = x * 3;
+		y = y * 5;
+
+	System.out.println(x);
+	System.out.println(y);
+
+	/*Resultado en consola D:
+		x = 30
+		y = 100
+	*/
+
+	System.out.println("E");
+
+		x = x / 2;
+		y = y / 4;
+
+	System.out.println(x);
+	System.out.println(y);
+
+	/*Resultado en consola E:
+		x = 15
+		y = 25
+	*/
+	}
+
+}
