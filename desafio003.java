c1a743a2f61d96bb17e7166045655edbc363adc8
Allan L. Castro <107632079+AllanCastro714@users.noreply.github.com>
2022-07-02 17:32:51 -0300

    Add files via upload

---
+++ desafio003.java
@@ -0,0 +1,64 @@
+
+public class desafio003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("A");
+		int x = 10;
+		int y = 20;
+		System.out.println(x);
+		System.out.println(y);
+		
+		/*Resultado en consola A:
+		x = 10
+		y = 20
+		 */
+		
+		System.out.println("B");
+		 x += 5;
+		 y -= 15;
+		System.out.println(x);
+		System.out.println(y);
+		
+		/*Resultado en consola B:
+		x = 15
+		y = 5
+		 */
+		
+		System.out.println("C");
+		x++;
+		y--;
+		System.out.println(x);
+		System.out.println(y);
+		
+		/*Resultado en consola C:
+		x = 16
+		y = 4
+		 */
+		
+		System.out.println("D");
+		x *= 4 ;
+		y *= -3 ;
+		System.out.println(x);
+		System.out.println(y);
+		
+		/*Resultado en consola D:
+		x = 64
+		y = -12
+		 */
+		
+		System.out.println("E");
+		x /= 2;
+		y /= 4 ;
+		System.out.println(x);
+		System.out.println(y);
+		
+		/*Resultado en consola E:
+		x = 32
+		y = -3
+		 */
+		
+	}
+
+}
---
+++ desafio004.java
@@ -0,0 +1,64 @@
+
+public class desafio004 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("A");
+		int x = 10;
+		int y=20;
+		System.out.println(x);
+		System.out.println(y);
+		
+		/*Resultado en consola A:
+		x = 10
+		y = 20
+		 */
+		
+		System.out.println("B");
+		 x =x+y;
+		 y =y+x;
+		System.out.println(x);
+		System.out.println(y);
+		
+		/*Resultado en consola B:
+		x = 30
+		y = 50
+		 */
+		
+		System.out.println("C");
+		x=x-y ;
+		y=y-x ;
+		System.out.println(x);
+		System.out.println(y);
+		
+		/*Resultado en consola C:
+		x = -20
+		y = 70
+		 */
+		
+		System.out.println("D");
+		x=x*y;
+		y=x*x;
+		System.out.println(x);
+		System.out.println(y);
+		
+		/*Resultado en consola D:
+		x = -1400
+		y = 1960000
+		 */
+		
+		System.out.println("E");
+		x=y/x ;
+		y=x/y ;
+		System.out.println(x);
+		System.out.println(y);
+		
+		/*Resultado en consola E:
+		x = -1400
+		y = 0
+		 */
+		
+	}
+
+}
